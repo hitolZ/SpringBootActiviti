002f936cf369048b28fb6bb0625bf405daacbc7f
CoderHitol <i@hitol.me>
2018-09-28 21:25:48 +0800

    SpringBoot2集成Activiti6

---
+++ src/main/java/com/hitol/springbootactiviti/activiticonfig/ActivitiConfig.java
@@ -0,0 +1,83 @@
+package com.hitol.springbootactiviti.activiticonfig;
+
+import org.activiti.engine.*;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+
+/**
+ * @author hitol
+ * @date 2018/9/28 下午9:19
+ */
+@Configuration
+public class ActivitiConfig {
+    @Autowired
+    PlatformTransactionManager platformTransactionManager;
+
+    @Autowired
+    DataSource dataSource;
+
+    @Bean
+    public SpringProcessEngineConfiguration getProcessEngineConfiguration() throws IOException {
+
+        System.out.println(dataSource);
+
+        SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+
+        configuration.setDataSource(dataSource);
+        configuration.setTransactionManager(platformTransactionManager);
+        configuration.setDatabaseType("mysql");
+        configuration.setDatabaseSchemaUpdate("true");
+        return configuration;
+    }
+
+    @Bean
+    public ProcessEngine processEngine() throws IOException {
+        SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+//        Resource[] resources = (Resource[]) new PathMatchingResourcePatternResolver().getResources(ResourceLoader.CLASSPATH_URL_PREFIX + "processes/*.bpmn");
+        configuration.setDataSource(dataSource);
+        configuration.setTransactionManager(platformTransactionManager);
+        configuration.setDatabaseType("mysql");
+        configuration.setDatabaseSchemaUpdate("true");
+//        configuration.setDeploymentResources(resources);
+        configuration.setDbIdentityUsed(false);
+        return configuration.buildProcessEngine();
+
+    }
+
+    @Bean
+    public RepositoryService repositoryService(ProcessEngine processEngine) {
+        return processEngine.getRepositoryService();
+    }
+
+    @Bean
+    public RuntimeService runtimeService(ProcessEngine processEngine) {
+        return processEngine.getRuntimeService();
+    }
+
+    @Bean
+    public TaskService taskService(ProcessEngine processEngine) {
+        return processEngine.getTaskService();
+    }
+
+    @Bean
+    public HistoryService historyService(ProcessEngine processEngine) {
+        return processEngine.getHistoryService();
+    }
+
+    @Bean
+    public ManagementService managementService(ProcessEngine processEngine) {
+        return processEngine.getManagementService();
+    }
+
+    @Bean
+    public IdentityService identityService(ProcessEngine processEngine) {
+        return processEngine.getIdentityService();
+    }
+}
